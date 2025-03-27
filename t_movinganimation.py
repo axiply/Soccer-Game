64c497e97c1165d3709ab26214503efd89e2df5b
Selina Yunfei <selinayunfei@gmail.com>
2025-03-27 09:21:56 -0400

    updates

--- t_movinganimation.py
+++ t_movinganimation.py
@@ -9,12 +9,13 @@ pygame.init()
 window = pygame.display.set_mode((1280, 960))
 clock = pygame.time.Clock()
 
-
-pos = pygame.math.Vector2(634,480)
+pos = pygame.math.Vector2(346,480)
 velocity = pygame.math.Vector2(2,0)
 
 soccerball = pygame.transform.scale(pygame.image.load('soccerball.png'),(50,50))
 image = soccerball.convert_alpha()
+
+status = "start screen"
 
 def start(SCREEN):
     """
@@ -37,23 +38,24 @@ def start(SCREEN):
 run = True
 while run:
     clock.tick(100)
-    start(window)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pos += velocity 
 
-    if pos.x + 25 >= 960 or pos.x - 25 <= 320:
-        velocity.x *= -1
-    if pos.y + 25 >= 960 or pos.y - 25 <= 320:
-        velocity.y *= -1
-           
-    window.fill(0)
-    #pygame.draw.lines(window, "gray", True, corner_points) 
-    image_rect = image.get_rect(center = (pos.x,pos.y))
-    window.blit(image, image_rect)
-    pygame.display.update()
+    if pos.x + 25 >= 960:
+        velocity.x = 0
+        window.fill(0)
+        pygame.display.update()
+    else:
+        window.fill(0)
+        image_rect = image.get_rect(center = (pos.x,pos.y))
+        window.blit(image, image_rect)
+        pygame.display.update()
+
+    if status == "start screen":
+        start(window)
 
 pygame.quit()
 exit()
